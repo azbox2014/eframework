421d71543d0356885743db1f20d04c9636b35982
acer <acer@book.com>
2019-10-25 19:38:23 +0800

    fwef

--- test/crawler.js
+++ test/crawler.js
@@ -37,8 +37,9 @@ class BookCrawler extends EventEmitter {
         nextOpt && self._fn_crawler_books(nextOpt);
         return Rx.from(urls);
       }),
+      RxOp.take(200),
       RxOp.mergeMap(url => self._fn_crawler_book(url))
-    );
+    ).subscribe(console.log, console.error);
     self._fn_crawler_books(self.options.entryUrl);
   }
 
@@ -80,7 +81,8 @@ class BookCrawler extends EventEmitter {
       });
     }).pipe(
       RxOp.map(({ bid, chapter_url_list }) => _(chapter_url_list).map(chapter_url => _.assign({}, { bid, chapter_url, idx: idx++ }).value())),
-      RxOp.mergeMap(chapter_info_list => Rx.from(chapter_info_list))
+      RxOp.mergeMap(chapter_info_list => Rx.from(chapter_info_list)),
+      RxOp.take(10)
     );
   }
 
@@ -104,55 +106,74 @@ class BookCrawler extends EventEmitter {
   _fn_crawler_chapter() { }
 }
 
-let crawler = new Crawler({
-  rateLimit: 2000,
-  maxConnections: 1,
-  jQuery: false,
-  userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
-});
+// let crawler = new Crawler({
+//   rateLimit: 2000,
+//   maxConnections: 1,
+//   jQuery: false,
+//   userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
+// });
 
-const OpCrawlerBook = uri => {
-  let book = {};
-  Rx.Observable.create(observer => {
-    crawler.queue({
-      uri,
-      callback: (err, res) => {
-        if (err) observer.error(err);
-        else if (res.statusCode == 200) observer.next(res);
-        else observer.error(res);
-        observer.complete();
-      }
-    });
-  }).pipe(
-    RxOp.map(res => {
-      let $ = JQuery.load(res.body);
-      book.title = $().text();
-      book.author = $().text();
-    })
-  );
-};
+// const OpCrawlerBook = uri => {
+//   let book = {};
+//   Rx.Observable.create(observer => {
+//     crawler.queue({
+//       uri,
+//       callback: (err, res) => {
+//         if (err) observer.error(err);
+//         else if (res.statusCode == 200) observer.next(res);
+//         else observer.error(res);
+//         observer.complete();
+//       }
+//     });
+//   }).pipe(
+//     RxOp.map(res => {
+//       let $ = JQuery.load(res.body);
+//       book.title = $().text();
+//       book.author = $().text();
+//     })
+//   );
+// };
 
-Rx.Observable.create(observer => {
-  crawler.queue({
-    uri: "http://mm.remenxs.com/type/",
-    callback: (err, res) => {
-      if (err) observer.error(err);
-      else if (res.statusCode == 200) observer.next(res);
-      else observer.error(res);
-      observer.complete();
+// Rx.Observable.create(observer => {
+//   crawler.queue({
+//     uri: "http://mm.remenxs.com/type/",
+//     callback: (err, res) => {
+//       if (err) observer.error(err);
+//       else if (res.statusCode == 200) observer.next(res);
+//       else observer.error(res);
+//       observer.complete();
+//     }
+//   });
+// }).pipe(
+//   RxOp.mergeMap(res => {
+//     let $ = JQuery.load(res.body);
+//     return Rx.from($(".main .list li"));
+//   }),
+//   RxOp.map(el => {
+//     let $el = JQuery.load(el);
+//     return URL.resolve("http://mm.remenxs.com/type/", $el(".bookname a").attr("href"));
+//   }),
+//   RxOp.mergeMap(url => {
+//     //
+//   })
+// ).subscribe(console.log, console.error);
+
+if (module.main == module) {
+  let options = {
+    entryUrl: "http://mm.remenxs.com/type/",
+    onBooks: (res) => {
+      //
+    },
+    onBook: (res) => {
+      //
+    },
+    onChapters: (res) => {
+      
+    },
+    onChapter: (res) => {
+      //
     }
-  });
-}).pipe(
-  RxOp.mergeMap(res => {
-    let $ = JQuery.load(res.body);
-    return Rx.from($(".main .list li"));
-  }),
-  RxOp.map(el => {
-    let $el = JQuery.load(el);
-    return URL.resolve("http://mm.remenxs.com/type/", $el(".bookname a").attr("href"));
-  }),
-  RxOp.mergeMap(url => {
-    //
-  })
-).subscribe(console.log, console.error);
-
+  };
+  let engine = new BookCrawler(options);
+  engine.start();
+}
