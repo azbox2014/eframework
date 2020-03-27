bf5d2b10a68d76d139d2114e47facdeb7bf780d3
zlq <zlq@mibook.com>
2020-03-28 00:42:04 +0800

    update

--- model/Book.js
+++ model/Book.js
@@ -1,19 +1,5 @@
-module.exports = (sequelize, DataTypes) => {
-  const { BIGINT, STRING, TEXT, INTEGER, DATE } = DataTypes;
-  const Book = sequelize.define('book',{
-    id: {
-      type: BIGINT.UNSIGNED,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    image: TEXT,
-    title: STRING(45),
-    author: STRING(45),
-    desc: TEXT,
-    status: INTEGER.UNSIGNED,  // 1.  上架; 2. 部分下架;  3. 部分上架;   4. 下架
-    remark: DataTypes.JSON,
-    created_time: DATE,
-    updated_titme: DATE
+module.exports = BF => {
+  return BF.model("Book", {
+    tableName: "books"
   });
-  return Book;
-}
+};
--- model/Chapter.js
+++ model/Chapter.js
@@ -1,19 +1,5 @@
-module.exports = (sequelize, DataTypes) => {
-  const { BIGINT, STRING, TEXT, INTEGER, DATE } = DataTypes;
-  const Chapter = sequelize.define('chapter', {
-    id: {
-      type: BIGINT.UNSIGNED,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    book_id: BIGINT.UNSIGNED,
-    title: STRING(45),
-    content: TEXT,
-    idx: INTEGER.UNSIGNED,
-    status: INTEGER.UNSIGNED,  // 1.  上架; 2. 部分下架;  3. 部分上架;   4. 下架
-    remark: DataTypes.JSON,
-    created_time: DATE,
-    updated_titme: DATE
-  });
-  return Chapter;
-}
+module.exports = BF => {
+    return BF.model("Chapter", {
+        tableName: "chapters"
+    })
+};
